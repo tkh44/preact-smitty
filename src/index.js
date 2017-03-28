63f2c5c6d7a41d7cc9242818096803a8875a2141
Kye Hohenberger <kye@sideway.com>
2017-03-28 14:07:38 -0600

    Use smitty inception to update children.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,63 @@
 import { Component, h } from 'preact'
+import { createStore } from 'smitty'
+
+function getId (a) {
+  return a
+    ? (a ^ Math.random() * 16 >> a / 4).toString(16)
+    : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, getId)
+}
+
+const tree = createStore({
+  instances: []
+})
+
+tree.createActions({
+  register: 'tree/REGISTER',
+  unregister: 'tree/UNREGISTER',
+  setUserStore: 'tree/SET_USER_STORE',
+  userStoreChange: 'tree/USER_STORE_CHANGE'
+})
+
+let calculateNextState = (userStoreState) => ({ id, instance, mapStateToProps, getProps }) => {
+  if (typeof mapStateToProps !== 'function') {
+    return
+  }
+
+  const nextState = mapStateToProps.length === 2
+    ? mapStateToProps(userStoreState, getProps())
+    : mapStateToProps(userStoreState)
+
+  if (instance.state !== nextState) {
+    instance.setState(nextState)
+  }
+}
+
+tree.handleActions({
+  [tree.actions.setUserStore]: (state, payload) => {
+    state.userStore = payload
+  },
+  [tree.actions.register]: (state, payload) => {
+    payload.instance.store = state.userStore
+    state.instances = state.instances.concat(payload)
+    calculateNextState(state.userStore.state)(payload)
+  },
+  [tree.actions.unregister]: (state, payload) => {
+    const index = state.instances.findIndex(inst => {
+      inst.id === payload
+    })
+    state.instances.splice(index, 1)
+  },
+  [tree.actions.userStoreChange]: (state, userStoreState) => {
+    const updater = calculateNextState(userStoreState)
+    state.instances.forEach(updater)
+  }
+})
 
 export class Provider extends Component {
-  getChildContext () {
-    return {
-      store: this.props.store
-    }
+  constructor (props) {
+    super(props)
+    tree.actions.setUserStore(props.store)
+    this.props.store.on('$$store:state:change', tree.actions.userStoreChange)
   }
 
   render (props) {
@@ -13,43 +66,31 @@ export class Provider extends Component {
 }
 
 export function connect (mapStateToProps) {
-  function mapState (props, context) {
-    return mapStateToProps(context.store.state, props)
-  }
-
   return function wrapComponent (WrappedComponent) {
     return class Connect extends Component {
-      constructor (props, context) {
-        super(props, context)
-        this.state = mapState(props, context)
-        this.handleStoreUpdate = this.handleStoreUpdate.bind(this)
-        this.context.store.on('$$store:state:change', this.handleStoreUpdate)
+      constructor (props) {
+        super(props)
+        this.id = getId()
+        tree.actions.register({
+          id: this.id,
+          instance: this,
+          mapStateToProps,
+          getProps: () => this.props
+        })
       }
 
       componentWillUnmount () {
-        window.cancelAnimationFrame(this.updateAnimId)
-        this.context.store.off('$$store:state:change', this.handleStoreUpdate)
+        tree.actions.unregister(this.id)
       }
 
       render (props, state) {
         return h(
           WrappedComponent,
-          Object.assign(
-            {},
-            Object.assign({}, props),
-            state,
-            { emit: this.context.store.emit }
-          )
+          Object.assign({}, Object.assign({}, props), state, {
+            emit: this.store.emit,
+            actions: this.store.actions
+          })
         )
-      }
-
-      handleStoreUpdate () {
-        if (typeof mapStateToProps !== 'function') return
-
-        this.updateAnimId = window.requestAnimationFrame(() => { // setState after all events have been handled
-          this.setState(mapState(this.props, this.context))
-          this.updateAnimId = null
-        })
       }
     }
   }
