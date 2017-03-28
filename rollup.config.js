d4c5541caca76e83ffce12be13889766a2a467e9
Kye Hohenberger <kye@sideway.com>
2017-03-28 14:22:21 -0600

    Update build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,10 @@
 import buble from 'rollup-plugin-buble'
 
-export default { useStrict: false, plugins: [buble()] }
+export default {
+  useStrict: false,
+  plugins: [buble()],
+  globals: {
+    preact: 'preact',
+    smitty: 'smitty'
+  }
+}
